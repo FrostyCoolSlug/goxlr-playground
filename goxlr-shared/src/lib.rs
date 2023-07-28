48a9b61a77ccd85094d6adcf6cb15ad16120f14c
Craig McLure <craig@mclure.net>
2023-07-28 19:52:02 +0100

    BREAKING EVERYTHING :D

---
+++ goxlr-shared/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod channels;
+pub mod colours;
+pub mod faders;
+pub mod encoders;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
