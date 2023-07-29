d2d6467ffa6f91704060a30b732de9115ae1e401
Craig McLure <craig@mclure.net>
2023-07-29 05:03:31 +0100

    Some minor updates..

--- goxlr-usb/src/interactions.rs
+++ goxlr-usb/src/interactions.rs
@@ -11,14 +11,39 @@ use enum_map::EnumMap;
 use goxlr_shared::interaction::{
     ButtonState, InteractiveButtons, InteractiveEncoders, InteractiveFaders,
 };
+use tokio::sync::mpsc;
 
 struct GoXLRState {
     buttonStates: EnumMap<InteractiveButtons, ButtonState>,
     volumeMap: EnumMap<InteractiveFaders, u8>,
     encoderMap: EnumMap<InteractiveEncoders, u8>,
+    // TODO: We need independent receivers for this struct, as well as an upstream sender.
+    // Under Linux, the receiver will be the timed poller, and under Windows the receiver will
+    // handle events received from TUSB. This class need to be generic, and simply handle the IO
+    // from other locations.
+    sender: mpsc::Sender<OutgoingChange>,
+    receiver: mpsc::Receiver<IncomingChange>,
 }
 
 // This needs to be platform specific, on Windows we use the TUSB event handler to trigger events,
 // where as under Linux, we use RUSB polling for events. We need to cleanly support both.
 
 impl GoXLRState {}
+
+// It's important not to map these together, under Linux with polling the 'Incoming' change may
+// match the existing value, we need to only trigger an outgoing change if the incoming != the
+// current value.
+enum IncomingChange {
+    ButtonDown(InteractiveButtons),
+    ButtonUp(InteractiveButtons),
+    VolumeChange(InteractiveFaders, u8),
+    EncoderChange(InteractiveEncoders, i8),
+}
+
+// The messages this struct will send upstream..
+enum OutgoingChange {
+    ButtonDown(InteractiveButtons),
+    ButtonUp(InteractiveButtons),
+    VolumeChange(InteractiveFaders, u8),
+    EncoderChange(InteractiveEncoders, i8),
+}
