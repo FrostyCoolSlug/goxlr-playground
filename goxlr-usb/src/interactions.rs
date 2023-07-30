f3ee3dbffd5d5746542dffcc28219e8c5e807391
Craig McLure <craig@mclure.net>
2023-07-31 00:40:10 +0100

    We only need a single ChangeEvent type here, that way messages can be passed verbatim if needed.

--- goxlr-usb/src/interactions.rs
+++ goxlr-usb/src/interactions.rs
@@ -17,22 +17,20 @@ struct GoXLRStateTracker {
     buttonStates: EnumMap<InteractiveButtons, ButtonState>,
     volumeMap: EnumMap<InteractiveFaders, u8>,
     encoderMap: EnumMap<InteractiveEncoders, u8>,
+
     // TODO: We need independent receivers for this struct, as well as an upstream sender.
     // Under Linux, the receiver will be the timed poller, and under Windows the receiver will
     // handle events received from TUSB. This class need to be generic, and simply handle the IO
     // from other locations.
-    sender: mpsc::Sender<OutgoingChange>,
-    receiver: mpsc::Receiver<IncomingChange>,
+    sender: mpsc::Sender<ChangeEvent>,
+    receiver: mpsc::Receiver<ChangeEvent>,
 }
 
 // This needs to be platform specific, on Windows we use the TUSB event handler to trigger events,
 // where as under Linux, we use RUSB polling for events. We need to cleanly support both.
 
 impl GoXLRStateTracker {
-    pub fn new(
-        receiver: mpsc::Receiver<IncomingChange>,
-        sender: mpsc::Sender<OutgoingChange>,
-    ) -> Self {
+    pub fn new(receiver: mpsc::Receiver<ChangeEvent>, sender: mpsc::Sender<ChangeEvent>) -> Self {
         Self {
             sender,
             receiver,
@@ -44,15 +42,7 @@ impl GoXLRStateTracker {
 // It's important not to map these together, under Linux with polling the 'Incoming' change may
 // match the existing value, we need to only trigger an outgoing change if the incoming != the
 // current value.
-enum IncomingChange {
-    ButtonDown(InteractiveButtons),
-    ButtonUp(InteractiveButtons),
-    VolumeChange(InteractiveFaders, u8),
-    EncoderChange(InteractiveEncoders, i8),
-}
-
-// The messages this struct will send upstream..
-enum OutgoingChange {
+enum ChangeEvent {
     ButtonDown(InteractiveButtons),
     ButtonUp(InteractiveButtons),
     VolumeChange(InteractiveFaders, u8),
