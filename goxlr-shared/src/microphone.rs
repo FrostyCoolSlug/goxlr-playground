9c56cde72a31c7b30185ca08b70152c6325aba92
Craig McLure <craig@mclure.net>
2024-04-14 01:17:58 +0100

    Removed 'MicType', '[XLR|Phantom|TRS]Gain' from Shared
    These should never be sent directly to the USB crate, instead SetMicGain should be used instead.

--- goxlr-shared/src/microphone.rs
+++ goxlr-shared/src/microphone.rs
@@ -1,3 +1,4 @@
+use crate::eq_frequencies::{Frequencies, MiniFrequencies};
 use enum_map::Enum;
 use serde::{Deserialize, Serialize};
 
@@ -5,7 +6,7 @@ use serde::{Deserialize, Serialize};
 pub enum MicrophoneType {
     XLR,
     Phantom,
-    TRS,
+    Jack,
 }
 
 /*
@@ -14,12 +15,8 @@ pub enum MicrophoneType {
  changes
 */
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum MicrophoneParamKeys {
-    MicType,
-    XLRGain,
-    PhantomGain,
-    TRSGain,
+#[derive(Debug, Hash, Copy, Clone, PartialEq, Eq)]
+pub enum MicParamKeys {
     GateThreshold,
     GateAttack,
     GateRelease,
@@ -49,8 +46,32 @@ pub enum MicrophoneParamKeys {
     Equalizer8KHzGain,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum MicrophoneEffectKey {
+impl MicParamKeys {
+    pub fn from_eq_gain(value: MiniFrequencies) -> Self {
+        match value {
+            MiniFrequencies::Eq90h => MicParamKeys::Equalizer90HzGain,
+            MiniFrequencies::Eq250h => MicParamKeys::Equalizer250HzGain,
+            MiniFrequencies::Eq500h => MicParamKeys::Equalizer500HzGain,
+            MiniFrequencies::Eq1kh => MicParamKeys::Equalizer1KHzGain,
+            MiniFrequencies::Eq3kh => MicParamKeys::Equalizer3KHzGain,
+            MiniFrequencies::Eq8kh => MicParamKeys::Equalizer8KHzGain,
+        }
+    }
+
+    pub fn from_eq_freq(value: MiniFrequencies) -> Self {
+        match value {
+            MiniFrequencies::Eq90h => MicParamKeys::Equalizer90HzFrequency,
+            MiniFrequencies::Eq250h => MicParamKeys::Equalizer250HzFrequency,
+            MiniFrequencies::Eq500h => MicParamKeys::Equalizer500HzFrequency,
+            MiniFrequencies::Eq1kh => MicParamKeys::Equalizer1KHzFrequency,
+            MiniFrequencies::Eq3kh => MicParamKeys::Equalizer3KHzFrequency,
+            MiniFrequencies::Eq8kh => MicParamKeys::Equalizer8KHzFrequency,
+        }
+    }
+}
+
+#[derive(Debug, Hash, Copy, Clone, PartialEq, Eq)]
+pub enum MicEffectKeys {
     MicInputMute,
     BleepLevel,
     GateMode,
@@ -159,4 +180,36 @@ pub enum MicrophoneEffectKey {
     Encoder2Enabled,
     Encoder3Enabled,
     Encoder4Enabled,
+}
+
+impl MicEffectKeys {
+    pub fn from_eq_gain(value: Frequencies) -> Self {
+        match value {
+            Frequencies::Eq31h => MicEffectKeys::Equalizer31HzGain,
+            Frequencies::Eq63h => MicEffectKeys::Equalizer63HzGain,
+            Frequencies::Eq125h => MicEffectKeys::Equalizer125HzGain,
+            Frequencies::Eq250h => MicEffectKeys::Equalizer250HzGain,
+            Frequencies::Eq500h => MicEffectKeys::Equalizer500HzGain,
+            Frequencies::Eq1kh => MicEffectKeys::Equalizer1KHzGain,
+            Frequencies::Eq2kh => MicEffectKeys::Equalizer2KHzGain,
+            Frequencies::Eq4kh => MicEffectKeys::Equalizer4KHzGain,
+            Frequencies::Eq8kh => MicEffectKeys::Equalizer8KHzGain,
+            Frequencies::Eq16kh => MicEffectKeys::Equalizer16KHzGain,
+        }
+    }
+
+    pub fn from_eq_freq(value: Frequencies) -> Self {
+        match value {
+            Frequencies::Eq31h => MicEffectKeys::Equalizer31HzFrequency,
+            Frequencies::Eq63h => MicEffectKeys::Equalizer63HzFrequency,
+            Frequencies::Eq125h => MicEffectKeys::Equalizer125HzFrequency,
+            Frequencies::Eq250h => MicEffectKeys::Equalizer250HzFrequency,
+            Frequencies::Eq500h => MicEffectKeys::Equalizer500HzFrequency,
+            Frequencies::Eq1kh => MicEffectKeys::Equalizer1KHzFrequency,
+            Frequencies::Eq2kh => MicEffectKeys::Equalizer2KHzFrequency,
+            Frequencies::Eq4kh => MicEffectKeys::Equalizer4KHzFrequency,
+            Frequencies::Eq8kh => MicEffectKeys::Equalizer8KHzFrequency,
+            Frequencies::Eq16kh => MicEffectKeys::Equalizer16KHzFrequency,
+        }
+    }
 }
