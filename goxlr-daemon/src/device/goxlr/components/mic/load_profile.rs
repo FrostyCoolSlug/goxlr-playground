9088be0ef86676a0e3f2cb0e0063788df9d11f0e
Craig McLure <craig@mclure.net>
2024-04-14 01:18:52 +0100

    Load Mic Type / Gain on profile load

--- goxlr-daemon/src/device/goxlr/components/mic/load_profile.rs
+++ goxlr-daemon/src/device/goxlr/components/mic/load_profile.rs
@@ -1,6 +1,7 @@
 use crate::device::goxlr::components::mic::mic_type::MicTypeCrate;
-use crate::device::goxlr::device::GoXLR;
 use anyhow::Result;
+
+use crate::device::goxlr::device::GoXLR;
 
 pub trait LoadMicProfile {
     async fn load_mic_profile(&mut self) -> Result<()>;
@@ -8,6 +9,8 @@ pub trait LoadMicProfile {
 
 impl LoadMicProfile for GoXLR {
     async fn load_mic_profile(&mut self) -> Result<()> {
+        self.apply_mic_gain().await?;
+
         //self.apply_mic_gain().await?;
 
         Ok(())
