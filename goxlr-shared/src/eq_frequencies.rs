b78a170374cf5cff7bb6f8b1790ccaaa20950421
Craig McLure <craig@mclure.net>
2024-04-14 01:09:02 +0100

    Allow EnumIter for EQ Frequencies

--- goxlr-shared/src/eq_frequencies.rs
+++ goxlr-shared/src/eq_frequencies.rs
@@ -1,7 +1,8 @@
 use enum_map::Enum;
 use serde::{Deserialize, Serialize};
+use strum::EnumIter;
 
-#[derive(Debug, Copy, Clone, Enum, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Enum, EnumIter, Serialize, Deserialize)]
 pub enum Frequencies {
     Eq31h,
     Eq63h,
@@ -15,7 +16,7 @@ pub enum Frequencies {
     Eq16kh,
 }
 
-#[derive(Debug, Copy, Clone, Enum, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Enum, EnumIter, Serialize, Deserialize)]
 pub enum MiniFrequencies {
     Eq90h,
     Eq250h,
